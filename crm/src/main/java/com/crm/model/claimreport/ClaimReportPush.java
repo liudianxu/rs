1aaf677edfee867d943a8e25608a170913f65c6a
liudianxu <ldx@LAPTOP-FU0594E5>
2019-06-27 23:28:15 +0800

    1

---
+++ crm/src/main/java/com/crm/model/claimreport/ClaimReportPush.java
@@ -0,0 +1,15 @@
+package com.crm.model.claimreport;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class ClaimReportPush extends Model<ClaimReportPush>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public static final ClaimReportPush dao = new ClaimReportPush();
+
+}

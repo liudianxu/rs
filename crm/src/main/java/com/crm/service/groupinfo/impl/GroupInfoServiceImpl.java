af89ea4f41298ddd0ebe999ca0fa98820b120fd3
liudianxu <ldx@LAPTOP-FU0594E5>
2019-08-25 14:01:20 +0800

    1

--- crm/src/main/java/com/crm/service/groupinfo/impl/GroupInfoServiceImpl.java
+++ crm/src/main/java/com/crm/service/groupinfo/impl/GroupInfoServiceImpl.java
@@ -41,17 +41,17 @@ public class GroupInfoServiceImpl implements GroupInfoService {
 		DataGrid<GroupInfo> datagrid = new DataGrid<>();
 		StringBuffer sql = new StringBuffer();
 		sql.append("select DISTINCT g.* from crm_group_info g ");
-		//sql.append("left join crm_customer_info c on c.group_id =g.id where 1=1");
+		sql.append("left join crm_customer_info c on c.group_id =g.id ");
 		if(StringUtils.isNotBlank(params.get("groupName"))){
 			sql.append(" and g.group_name like '%").append(params.get("groupName")).append("%' ");
 		}
 		if(StringUtils.isNotBlank(params.get("certNo"))){
 			sql.append(" and g.cert_no ="+params.get("certNo"));
 		}
-		/*if(StringUtils.isNotBlank(customerIds)){
-			sql.append(" and c.id in ("+customerIds+")");
-		}*/
-		sql.append(" order by g.create_time desc ");
+		if(StringUtils.isNotBlank(customerIds)){
+			sql.append(" where c.id in ("+customerIds+")");
+		}
+		sql.append("  order by g.create_time desc ");
 		SqlPara sqlPara = new SqlPara();
 		sqlPara.setSql(sql.toString());
 		Page<GroupInfo> groupInfos = GroupInfo.dao.paginate(page.getPageNumber(), page.getPageSize(), sqlPara);
@@ -176,7 +176,7 @@ public class GroupInfoServiceImpl implements GroupInfoService {
 		sql.append("left join crm_customer_info c on c.group_id =g.id  ");
 		//sql.append("left join crm_group_insurance_orders o on o.insure_customer_id =c.id where 1=1");
 		if(StringUtils.isNotBlank(customerIds)){
-			sql.append(" and c.id in ("+customerIds+")");
+			sql.append(" where c.id in ("+customerIds+")");
 		}
 		sql.append(" order by g.create_time desc ");
 		SqlPara sqlPara = new SqlPara();
